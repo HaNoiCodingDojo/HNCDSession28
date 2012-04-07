8fe5566eb188aa3e6fd930eb2f4f1b6c8e7b23d6
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2012-04-07 14:43:33 +0700

    TBBle

--- BrainFuckInterpreter.py
+++ BrainFuckInterpreter.py
@@ -7,5 +7,10 @@ class BrainfuckInterpreterTest(unittest.TestCase):
 	def testTruth(self):
 		self.assertEquals(True,True)
 
+	def testEmptyProgram(self):
+		interpreter = BrainfuckInterpreter()
+		result = interpreter.run( "" )
+		self.assertEquals( "", result )
+
 if __name__ == "__main__":
 	unittest.main()
