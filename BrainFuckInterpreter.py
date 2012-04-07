927af8277125609a1b292497ca35ce7d33632532
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2012-04-07 14:40:11 +0700

    SergeStinckwich

---
+++ BrainFuckInterpreter.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import unittest
+
+class BrainfuckInterpreterTest(unittest.TestCase):
+
+	def testTruth(self):
+		self.assertEquals(True,True)
+
+if __name__ == "__main__":
+	unittest.main()
