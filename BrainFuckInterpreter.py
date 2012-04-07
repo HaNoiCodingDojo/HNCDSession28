a09709d8b957fbc7a554dcb2c9e54a664efcabe5
Serge Stinckwich <Serge.Stinckwich@gmail.com>
2012-04-07 15:27:54 +0700

    SergeStinckwich

--- BrainFuckInterpreter.py
+++ BrainFuckInterpreter.py
@@ -11,7 +11,7 @@ class BrainfuckInterpreter( object ):
 		if program == ",..,..":
 			self.data = str(input[self.iPointer])*2
 			self.iPointer +=1
-			self.data = self.data+str(input[self.iPointer])*2
+			self.data += str(input[self.iPointer])*2
 		elif program == ",.":
 			self.data = str(input[self.iPointer])
 		elif len(program) == 0:
